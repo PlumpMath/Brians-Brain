80aa9008116c2278174afc275358ae012cd7af27
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-07-29 23:32:05 +0800

    clean code
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- src/clj/brains_brain/core.clj
+++ src/clj/brains_brain/core.clj
@@ -21,63 +21,22 @@
         (* (- cell-size 1)
            (- board-size cell-size)))))
 
-(println (str "size: " size))
-
-(vec (repeat 9 0))
-
-(def a (vec (replicate 8 (vec (replicate 8 :off)))))
-
-(nth (nth a 1 ) 1)
-
 ;; We can use clojure's `make-array' create 2d array, but we do our own here
+;; and convert to array object
 (defn make-array
-  [width height init-val]
-  (vec
-   (map vec
-       (partition width (repeat (* width height) init-val)))))
+  "Create 2d-array object."
+  ([width height] (make-array width height 0))
+  ([width height init-val]
+   (to-array-2d
+    (partition width (repeat (* width height) init-val)))))
 
-(defn array-get
-  [arr i j]
-  (nth (nth arr i) j))
-
-(def a (make-array 8 4 :off))
-
-(def a (make-array 8 8 :off))
-
-(map vec a)
-(assoc [0 1 2] 1 5)
-
-(assoc (seq (nth a 3)) 2 2)
-
-(var-set '(1 2 3) 1 )
-
-(assoc a 0 (assoc (nth a 1) 0 1))
-
-(array-get a 1 1)
-
-(defn arr-set
-  [arr i j val]
-  (nth arr i)
-  )
-
-(for [y (range 8) x (range 8)] :off );; {:x x :y y})
-;;(partition  8 (for [y (range 8) x (range 8)] {:x x :y y}))
-(nth (partition 5 (repeat 25 0)) 1 )
-
-
-(nth (nth a 0) 1)
-
-(assoc a)
-
-(arr-get a 1 1)
-
-;; TODO: this function need a little modify to run on cljs
 (defn make-board
+  "Create board with random initial value"
   []
-  (let [board (make-array Integer/TYPE board-size board-size)]
+  (let [board (make-array board-size board-size)]
     (doseq [x (range 0 board-size)
             y (range 0 board-size)]
-      (aset-int board x y (if (< (rand-int 100) 50) 0 1)))
+      (aset board x y (if (< (rand-int 100) 50) :off :on)))
     board))
 
 (def a (make-board))
@@ -91,7 +50,7 @@
     (draw g
           (rect xb yb cell-size)
           (style :background (case cell
-                               1 :red
+                               :on :red
                                :white) ; default (:off)
                  :foreground :black ; broader
                  ))))
